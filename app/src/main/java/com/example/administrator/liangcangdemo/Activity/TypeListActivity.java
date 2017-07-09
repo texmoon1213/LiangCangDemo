c56d19ff0de635bed08a2d0868a14c6c716bbe70
texmoon1213 <texmoon@163.com>
2017-07-09 14:51:02 +0800

    初步实现商品列表页面popuowindow和点击事件

--- app/src/main/java/com/example/administrator/liangcangdemo/Activity/TypeListActivity.java
+++ app/src/main/java/com/example/administrator/liangcangdemo/Activity/TypeListActivity.java
@@ -1,14 +1,18 @@
 package com.example.administrator.liangcangdemo.Activity;
 
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -29,7 +33,7 @@ import butterknife.ButterKnife;
 import okhttp3.Call;
 import okhttp3.Response;
 
-public class TypeListActivity extends AppCompatActivity {
+public class TypeListActivity extends AppCompatActivity implements View.OnClickListener {
 
     @BindView(R.id.search_titlebar)
     ImageView searchTitlebar;
@@ -43,8 +47,24 @@ public class TypeListActivity extends AppCompatActivity {
     LinearLayout llTypelist;
     @BindView(R.id.recycle_type_list)
     RecyclerView recycleTypeList;
+    //    @BindView(R.id.pop_all)
+//    TextView popAll;
+//    @BindView(R.id.pop_200)
+//    TextView pop200;
+//    @BindView(R.id.pop_500)
+//    TextView pop500;
+//    @BindView(R.id.pop_1000)
+//    TextView pop1000;
+//    @BindView(R.id.pop_3000)
+//    TextView pop3000;
     private ShopTypeBean.DataBean.ItemsBean type_bean;
     private String itemurl;
+    private PopupWindow popupWindow;
+    private TextView popAll;
+    private TextView pop200;
+    private TextView pop500;
+    private TextView pop1000;
+    private TextView pop3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,12 +73,41 @@ public class TypeListActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         initData();
         initView();
+        initListener();
+    }
+
+    private void initListener() {
+        llTypelist.setOnClickListener(this);
+        popAll.setOnClickListener(this);
+        pop200.setOnClickListener(this);
+        pop500.setOnClickListener(this);
+        pop1000.setOnClickListener(this);
+        pop3000.setOnClickListener(this);
+    }
+
+    private void showPopupWindow() {
+        popupWindow.showAsDropDown(llTypelist);
     }
 
     private void initView() {
         searchTitlebar.setVisibility(View.GONE);
         backTitlebar.setVisibility(View.VISIBLE);
         tvTitlebar.setText("商店");
+
+        popupWindow = new PopupWindow(TypeListActivity.this);
+
+        popupWindow.setWidth(ViewGroup.LayoutParams.MATCH_PARENT);
+        popupWindow.setHeight(ViewGroup.LayoutParams.WRAP_CONTENT);
+        View inflate = LayoutInflater.from(this).inflate(R.layout.popuwindow_type_item, null);
+        popupWindow.setContentView(inflate);
+        popupWindow.setBackgroundDrawable(new ColorDrawable(0x00000000));
+        popupWindow.setOutsideTouchable(false);
+        popupWindow.setFocusable(true);
+        popAll = (TextView) inflate.findViewById(R.id.pop_all);
+        pop200 = (TextView) inflate.findViewById(R.id.pop_200);
+        pop500 = (TextView) inflate.findViewById(R.id.pop_500);
+        pop1000 = (TextView) inflate.findViewById(R.id.pop_1000);
+        pop3000 = (TextView) inflate.findViewById(R.id.pop_3000);
     }
 
     private void initData() {
@@ -108,6 +157,64 @@ public class TypeListActivity extends AppCompatActivity {
         }
     }
 
+
+    //    @OnClick({R.id.ll_typelist, R.id.pop_all, R.id.pop_200, R.id.pop_500, R.id.pop_1000, R.id.pop_3000})
+//    public void onViewClicked(View view) {
+//        switch (view.getId()) {
+//            case R.id.ll_typelist:
+//                showPopupWindow();
+//                break;
+//            case R.id.pop_all:
+//                Toast.makeText(TypeListActivity.this, "all", Toast.LENGTH_SHORT).show();
+//                popAll.setBackgroundResource(R.drawable.daren);
+//                popupWindow.dismiss();
+//                break;
+//            case R.id.pop_200:
+//                popAll.setBackgroundResource(R.drawable.daren);
+//                popupWindow.dismiss();
+//                break;
+//            case R.id.pop_500:
+//                popAll.setBackgroundResource(R.drawable.daren);
+//                popupWindow.dismiss();
+//                break;
+//            case R.id.pop_1000:
+//                popAll.setBackgroundResource(R.drawable.daren);
+//                popupWindow.dismiss();
+//                break;
+//            case R.id.pop_3000:
+//                popAll.setBackgroundResource(R.drawable.daren);
+//                popupWindow.dismiss();
+//                break;
+//        }
+//    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.ll_typelist:
+                showPopupWindow();
+                break;
+            case R.id.pop_all:
+                Toast.makeText(TypeListActivity.this, "all", Toast.LENGTH_SHORT).show();
+                popAll.setBackgroundResource(R.drawable.daren);
+                break;
+            case R.id.pop_200:
+                pop200.setBackgroundResource(R.drawable.daren);
+                break;
+            case R.id.pop_500:
+                pop500.setBackgroundResource(R.drawable.daren);
+                break;
+            case R.id.pop_1000:
+                pop1000.setBackgroundResource(R.drawable.daren);
+                break;
+            case R.id.pop_3000:
+                pop3000.setBackgroundResource(R.drawable.daren);
+                Toast.makeText(TypeListActivity.this, "3000", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+
+
     private class TypeListener implements ShopTypeListRecycleAdapter.ItemClickListener {
         private final List<ShopTypeListBean.DataBean.ItemsBean> itemsBean;
 
@@ -123,4 +230,5 @@ public class TypeListActivity extends AppCompatActivity {
             startActivity(initent);
         }
     }
+
 }
